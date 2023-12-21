0e2e4755fc1b96449bc8b7932e15068ee0e1cb02
Nehemie BALUKIDI <alfred.nehemie@gmail.com>
2023-12-21 16:12:37 +0200

    adds constraintes(first part)

--- jour_2.sql
+++ jour_2.sql
@@ -177,4 +177,52 @@ CASE
 	WHEN price > 12 THEN 'Abordable'
 	ELSE 'Moins cher'
 END
-AS "Avis sur prix" FROM 
+AS "Avis sur prix" FROM
+
+--- Contraintes
+DROP TABLE IF EXISTS books_authors;
+DROP TABLE IF EXISTS authors;
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE IF NOT EXISTS books (
+	id SERIAL PRIMARY KEY,
+	title VARCHAR(150) NOT NULL,
+	year_published SMALLINT NOT NULL,
+	publisher VARCHAR(50) NOT NULL,
+	price NUMERIC(6,2) NOT NULL CHECK(price>0)
+);
+
+CREATE TABLE IF NOT EXISTS authors (
+	id SERIAL PRIMARY KEY,
+	first_name VARCHAR(100) NOT NULL,
+	last_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS books_authors (
+	book_id INTEGER REFERENCES books,
+	author_id INTEGER REFERENCES authors
+);
+
+
+INSERT INTO books (title, year_published, publisher, price) VALUES 
+('Introduction au SQL', 2000, 'Packt', 15.49),
+('La methode Agile', 2005, 'Oreilly', 23.99),
+('Git & GitHub', 2020, 'Oreilly', 41.99),
+('SQL pour l''analyse des données', 2021, 'Oreilly', 59.99),
+('Le CSS', 2014, 'Wiley', 15.99),
+('SQL : éléments internes de la base de données', 2018, 'Packt', 63.75),
+('Java: Introduction', 2014,'Wiley', 11.99),
+('Laravel pour les nuls', 2012, 'Apress', 23.99),
+('L''art du SQL', 2015, 'Wiley', 27.75);
+
+
+INSERT INTO authors (first_name, last_name) VALUES ('Xavier', 'Dupont'), ('Christophe', 'Laporte'), ('Pascal', 'Louis'), ('Claire', 'Martin');
+
+SELECT * FROM books;
+SELECT * FROM authors;
+SELECT * FROM books_authors;
+
+SELECT * FROM books WHERE id = 3;
+
+INSERT INTO books_authors (book_id, author_id) VALUES (1, 3), (4, 4),(5, 4),(3, 1),(7, 3),(8, 3),(6, 3),(8, 3);
+
